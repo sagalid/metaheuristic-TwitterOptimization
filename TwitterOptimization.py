fd426aa76d6297c7c3565069bb8d24e479affcda
agustin <jsalasf@inacap.cl>
2018-11-05 16:29:05 -0300

    se agregan las funcionalidades de tweet, fitness y feasible.

--- TwitterOptimization.py
+++ TwitterOptimization.py
@@ -2,56 +2,130 @@ from termcolor import colored
 import sys
 import getopt
 import ParsingFile as pf
+import random
+from scipy.stats import bernoulli
+import numpy as np
 verbose = False
 population_number_M = 30
 number_of_followers_F = 10
 number_of_evaluation_N = 10000
-twitter_users = [None]
-
+bernoulli_p = 0.5
+users_of_twitter = [None]
+hottest_tweet = [None]
+followers = dict()
+tweets_by_user = dict()
+seed = None
 
 def run_mh(inputFile):
-    global twitter_users
-    if(verbose):
+    global hottest_tweet
+    initialize_mh(inputFile)
+    for ronda in range(number_of_evaluation_N):
+        for person in users_of_twitter:
+            followers = obtain_followers(person)
+            tweets = obtain_twitters(followers)
+            re_tweets = obtain_re_twitts(followers)
+            best_tweet = obtain_best(tweets, re_tweets)
+            worst_tweet = obtain_worst(tweets, re_tweets)
+            perform_re_tweet(best_tweet)
+            # TODO uncoment
+            #if (fitness(best_tweet) < fitness(hottest_tweet)):
+            #    hottest_tweet = best_tweet
+            follow_author(best_tweet)
+            unfollow_author(worst_tweet)
+            # TODO se debe programar esta parte.
+            ##if is own tweet hasnt been retweeted for several rounds then
+            ####Tweet a new tweet B and replace hottest tweet with B if B is more valuable...
+
+def initialize_mh(inputFile):
+    global users_of_twitter
+    global seed
+    if (verbose):
         print(colored('\n\nBegin of Twitter Optmization\n', 'blue'))
-    if(verbose):
+    if (verbose):
         print(colored('Parsing file...', 'yellow'), end='')
-    if(pf.parsear(inputFile) and verbose):
-            print(colored('OK\n', 'blue'))
-    if(verbose):
+    if (pf.parsear(inputFile) and verbose):
+        print(colored('OK\n', 'blue'))
+    if (verbose):
         print(colored("Rows:\t\t", 'yellow'), colored(pf.filas, 'blue'))
         print(colored("Columns:\t", 'yellow'), colored(pf.columnas, 'blue'), '\n')
         print(colored(pf.matriz_a, 'yellow'))
-    if(verbose):
-        print(colored('\nInitial twitter user...', 'yellow'), end='')
-    twitter_users = initial_twitter_user()
+    if (verbose):
+        print(colored('\nSeed Generation...', 'yellow'), end='')
+    seed = random.randint(0, (population_number_M-1))
+    random.seed(seed)
+    if (verbose):
+        print(colored('OK\n', 'blue'))
+    if (verbose):
+        print(colored('Initial twitter user...', 'yellow'), end='')
+    users_of_twitter = initial_twitter_user()
     if (verbose):
         print(colored('OK', 'blue'))
+        print(users_of_twitter)
     if (verbose):
         print(colored('\nInitial Following...', 'yellow'), end='')
-    initial_following()
+    initial_following(users_of_twitter)
     if (verbose):
         print(colored('OK', 'blue'))
+        print(followers)
     if (verbose):
-        print(colored('\nRandom Twitt...', 'yellow'), end='')
+        print(colored('\nRandom Tweett...', 'yellow'), end='')
     random_twitt()
     if (verbose):
         print(colored('OK', 'blue'))
     if (verbose):
-        print(colored('\nRandom Re-Twitt...', 'yellow'), end='')
+        print(colored('\nRandom Re-Tweett...', 'yellow'), end='')
     random_re_twitt()
     if (verbose):
         print(colored('OK', 'blue'))
     if (verbose):
-        print(colored('\nFind hottest Twitt...', 'yellow'), end='')
+        print(colored('\nFind hottest Tweett...', 'yellow'), end='')
     find_hottest_twitt()
     if (verbose):
         print(colored('OK', 'blue'))
 
-    for ronda in range(number_of_evaluation_N):
-        for person in twitter_users:
-            followers = obtain_followers(person)
-            twitts = obtain_twitters(followers)
-            re_twitts = obtain_re_twitts(followers)
+
+def fitness(tweet):
+    if tweet is not None:
+        cost_vector = pf.getVectorCosto()
+        activated_vector = [a * b for a, b in zip(tweet, cost_vector)]
+        return sum(activated_vector)
+    else:
+        return 0
+
+
+def feasible(tweet):
+    a_transposed = np.transpose(pf.getMatrizA())
+    check_feasible = np.dot(tweet, a_transposed)
+    cumple = True
+
+    for restriccion in check_feasible:
+        if restriccion == 0:
+            cumple = False
+
+    if cumple:
+        return True
+    else:
+        return False
+
+
+def follow_author(best_tweet):
+    pass
+
+
+def unfollow_author(worst_tweet):
+    pass
+
+
+def perform_re_tweet(best_tweet):
+    pass
+
+
+def obtain_worst(tweets, re_tweets):
+    pass
+
+
+def obtain_best(tweets, re_tweets):
+    pass
 
 
 def obtain_re_twitts(followers):
@@ -76,12 +150,26 @@ def initial_twitter_user():
     return [i for i in range(population_number_M)]
 
 
-def initial_following():
-    pass
+def initial_following(twitter_users):
+    global followers
+    for user in twitter_users:
+        followers_list = []
+        for i in range(number_of_followers_F):
+            random_user = random.randint(0, (population_number_M-1))
+            if followers_list.__len__() == 0:
+                followers_list.append(random_user)
+            else:
+                while random_user in followers_list:
+                    random_user = random.randint(1, (population_number_M-1))
+                followers_list.append(random_user)
+        followers[user] = followers_list
 
 
 def random_twitt():
-    pass
+    global tweets_by_user
+    users = [i for i in range(population_number_M)]
+    for user in users:
+        tweets_by_user[user] = list(bernoulli.rvs(bernoulli_p, size=pf.getCantidadColumnas()))
 
 
 def random_re_twitt():
