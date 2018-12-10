80d4b960d74b7df4d20bc2236e542e19354649e4
agustin <jsalasf@inacap.cl>
2018-12-10 17:03:42 -0300

    listo en su mayoria. avanzar con los TODO.

--- TwitterOptimization.py
+++ TwitterOptimization.py
@@ -5,13 +5,14 @@ import ParsingFile as pf
 import random
 from scipy.stats import bernoulli
 import numpy as np
+
 verbose = False
 population_number_M = 30
 number_of_followers_F = 10
-number_of_evaluation_N = 10000
+number_of_evaluation_N = 100
 bernoulli_p = 0.5
-users_of_twitter = [None] # List of M twitter users.
-#hottest_tweet = [None]
+
+users_of_twitter = [None]  # List of M twitter users.
 followers = dict()  # Each user has F followers.
 tweets_by_user = dict()  # Each user has a tweet and every tweet has: user [key], tweeter, fitness, factibility and Number of RT.
 hottest_tweet_by_user = dict()  # Tweets with more RT. Could exist more than one, given equal number of RT.
@@ -19,21 +20,16 @@ seed = None
 
 
 def run_mh(inputFile):
-    global hottest_tweet
     initialize_mh(inputFile)
     for rund in range(number_of_evaluation_N):
         for person in users_of_twitter:
             user_followers = obtain_followers(person)
             tweets = obtain_twitters(user_followers)  # Get tweets and retweets
-            # re_tweets = obtain_re_tweets(followers)
             best_tweet = obtain_best(tweets)
             worst_tweet = obtain_worst(tweets)
             perform_re_tweet(best_tweet)
-            # TODO uncoment
-            #if (fitness(best_tweet) < fitness(hottest_tweet)):
-            #    hottest_tweet = best_tweet
-            follow_author(best_tweet)
-            unfollow_author(worst_tweet)
+            follow_author(person, best_tweet)
+            unfollow_author(person, worst_tweet)
             # TODO se debe programar esta parte.
             ##if is own tweet hasnt been retweeted for several rounds then
             ####Tweet a new tweet B and replace hottest tweet with B if B is more valuable...
@@ -58,7 +54,7 @@ def initialize_mh(inputFile):
         print(colored(pf.matriz_a, 'yellow'))
     if (verbose):
         print(colored('\nSeed Generation...', 'yellow'), end='')
-    seed = random.randint(0, (population_number_M-1))
+    seed = random.randint(0, (population_number_M - 1))
     random.seed(seed)
     if (verbose):
         print(colored('OK\n', 'blue'))
@@ -67,13 +63,13 @@ def initialize_mh(inputFile):
     users_of_twitter = initial_twitter_user()
     if (verbose):
         print(colored('OK', 'blue'))
-        #print(users_of_twitter)
+        # print(users_of_twitter)
     if (verbose):
         print(colored('\nInitial Following...', 'yellow'), end='')
     initial_following(users_of_twitter)
     if (verbose):
         print(colored('OK', 'blue'))
-        #print(followers)
+        # print(followers)
     if (verbose):
         print(colored('\nRandom Tweett...', 'yellow'), end='')
     random_twitt()
@@ -115,64 +111,153 @@ def feasible(tweet):
         return False
 
 
-def follow_author(best_tweet):
-    pass
+def follow_author(person, best_tweet):
+    """
+
+    :param best_tweet:
+    :return:
+    """
+    global followers
+    followers_by_user = followers[person]
+    for user in best_tweet:
+        follow_user = user
+
+    tmp_fitness = None
+    tmp_user = None
+    for follow in followers_by_user:
+        c_fitness = tweets_by_user[follow][1]
+        if tmp_fitness is None:
+            tmp_fitness = c_fitness
+        elif tmp_fitness < c_fitness:
+            tmp_fitness = c_fitness
+            tmp_user = follow
+
+    if follow_user not in followers_by_user:
+        followers_by_user.append(follow_user)
+        followers_by_user.remove(tmp_user)
 
 
-def unfollow_author(worst_tweet):
-    pass
+def unfollow_author(person, worst_tweet):
+    """
+    worst_tweet = user [key], tweeter, fitness, factibility and Number of RT
+    :param worst_tweet:
+    :param user_followers:
+    :return:
+    """
+    global followers
+    followers_by_user = followers[person]
+    for user in worst_tweet:
+        unfollow_user = user
+
+    for i in range(number_of_followers_F):
+        random_user = random.randint(0, (population_number_M - 1))
+        if (random_user != unfollow_user) and (random_user not in followers_by_user):
+            break
+    list_for_replace = []
+    for user in followers_by_user:
+        if user == unfollow_user:
+            list_for_replace.append(random_user)
+        else:
+            list_for_replace.append(user)
+    followers[person] = list_for_replace
 
 
 def perform_re_tweet(best_tweet):
-    pass
+    """
+    Retweet the best tweet in the followers 
+    user [key], tweeter, fitness, factibility and Number of RT
+    :param best_tweet:
+    :return:
+    """
+    global tweets_by_user
+    user_number = 0
+    for user in best_tweet:
+        user_number = user
+
+    rt_number = tweets_by_user[user_number][3]
+    rt_number += 1  # Retweet by increment in 1.
+    tweets_by_user[user_number][3] = rt_number
 
 
 def obtain_worst(tweets):
     """
-
+    obtain worst tweet by fitness from followers.
+    user [key], tweeter, fitness, factibility and Number of RT
     :param tweets:
     :return dictionary whit worst tweet, user[key], tweet, fitness and #RT:
     """
     max_fitness = None
     fitness_list = []
     for tweet in tweets:
-        current_fitness = tweets[tweet][1]
-        fitness_list.append(current_fitness)
+        c_fitness = tweets[tweet][1]
+        c_factibility = tweets[tweet][2]
+        if c_factibility:
+            fitness_list.append(c_fitness)
     max_fitness = max(fitness_list)
     worst_tweet = dict()
     for tweet in tweets:
-        if tweets[tweet][1] == max_fitness:
-            worst_tweet[tweet] = tweets[tweet][0], tweets[tweet][1], tweets[tweet][2]
+        c_tweet = tweets[tweet][0]
+        c_fitness = tweets[tweet][1]
+        c_factibility = tweets[tweet][2]
+        c_number_of_rt = tweets[tweet][3]
+        if c_fitness == max_fitness:
+            worst_tweet[tweet] = [c_tweet, c_fitness, c_factibility, c_number_of_rt]
     return worst_tweet
 
 
 def obtain_best(tweets):
     """
     obtain the best tweet by fitness from followers.
+    user [key], tweeter, fitness, factibility and Number of RT
     :param tweets:
     :return dictionary whit best tweet, user[key], tweet, fitness and #RT:
     """
+    global hottest_tweet_by_user
     min_fitness = None
     fitness_list = []
     for tweet in tweets:
         current_fitness = tweets[tweet][1]
-        fitness_list.append(current_fitness)
+        factibility = tweets[tweet][2]
+        if factibility:
+            fitness_list.append(current_fitness)
     min_fitness = min(fitness_list)
     best_tweet = dict()
     for tweet in tweets:
-        if tweets[tweet][1] == min_fitness:
-            best_tweet[tweet] = tweets[tweet][0], tweets[tweet][1], tweets[tweet][2]
+        c_tweet = tweets[tweet][0]
+        c_fitness = tweets[tweet][1]
+        c_factibility = tweets[tweet][2]
+        c_number_of_rt = tweets[tweet][3]
+        if c_fitness == min_fitness:
+            best_tweet[tweet] = [c_tweet, c_fitness, c_factibility, c_number_of_rt]
+            for x in hottest_tweet_by_user:
+                if hottest_tweet_by_user[x][1] > c_fitness:
+                    hottest_tweet_by_user = None
+                    hottest_tweet_by_user = dict()
+                    hottest_tweet_by_user = best_tweet
     return best_tweet
 
 
 def obtain_twitters(followers):
+    """
+    user [key], tweeter, fitness, factibility and Number of RT
+    :param followers:
+    :return:
+    """
     tweets = dict()
     for person in followers:
-        tweets[person] = [tweets_by_user[person][0], tweets_by_user[person][1], tweets_by_user[person][3]]
+        tweets[person] = [tweets_by_user[person][0],  # Tweeter
+                          tweets_by_user[person][1],  # Fitness
+                          tweets_by_user[person][2],  # Factibility
+                          tweets_by_user[person][3]]  # RT Num
     return tweets
 
 
 def obtain_followers(person):
+    """
+    This function return the followers of person P.
+    :param person:
+    :return list of followers:
+    """
     return followers[person]
 
 
@@ -196,12 +281,12 @@ def initial_following(twitter_users):
     for user in twitter_users:
         followers_list = []
         for i in range(number_of_followers_F):
-            random_user = random.randint(0, (population_number_M-1))
+            random_user = random.randint(0, (population_number_M - 1))
             if followers_list.__len__() == 0:
                 followers_list.append(random_user)
             else:
                 while random_user in followers_list:
-                    random_user = random.randint(1, (population_number_M-1))
+                    random_user = random.randint(1, (population_number_M - 1))
                 followers_list.append(random_user)
         followers[user] = followers_list
 
@@ -225,7 +310,7 @@ def random_twitt():
         # in tweets by user, append 4 principal elements: tweet, fitness, feasibility and number of re-tweet.
         tweets_by_user[user] = [random_tweet, fitness_of_tweet, is_feasible, 0]
 
-    #print(tweets_by_user)
+    # print(tweets_by_user)
 
 
 def random_re_twitt():
@@ -233,42 +318,52 @@ def random_re_twitt():
     for user in tweets_by_user.keys():
         followers_of_user = obtain_followers(user)
         random_follower = random.choice(followers_of_user)
-        while(1):
+        while (1):
             tweet_details = tweets_by_user[random_follower]
             if tweet_details[2]:
                 break
             else:
                 random_follower = random.choice(followers_of_user)
         tweet_details[3] += 1
-        #print(tweets_by_user[user][3])
+        # print(tweets_by_user[user][3])
+
+
+def find_hottest_by_fitness():
+    """
+    This function search and return de best tweet (Binary Vector) for fitness and RT's.
+    :return Tweet binary vector:
+    """
+    pass
 
 
 def find_hottest_twitt():
     """
     This function iterate over every twitter in tweets_by_user,
-    in look for those with better fitness .
+    in look for the one with best fitness.
     :return:
     dictionary whit this estructure
-    [user], fitness, feasible, number of retweets.
-
+    [user], tweet,  fitness, feasible, # RT.
     """
-    max_rt = 0
     global hottest_tweet_by_user
+    best_fitness = None
     for user in tweets_by_user:
-        number_of_retweets = tweets_by_user[user][3]
-        factible = tweets_by_user[user][2]
-        if number_of_retweets >= max_rt and factible:
-            max_rt = number_of_retweets
-    # This line, will delete previous hottest tweets.
+        c_fitness = tweets_by_user[user][1]
+        c_factibility = tweets_by_user[user][2]
+        if best_fitness is None and c_factibility:
+            best_fitness = c_fitness
+        elif c_fitness < best_fitness and c_factibility:
+            best_fitness = c_fitness
+    temp_hottest = dict()
+    for user in tweets_by_user:
+        c_tweet = tweets_by_user[user][0]
+        c_fitness = tweets_by_user[user][1]
+        c_factibility = tweets_by_user[user][2]
+        c_number_of_rt = tweets_by_user[user][3]
+        if c_fitness == best_fitness:
+            temp_hottest[user] = [c_tweet, c_fitness, c_factibility, c_number_of_rt]
+    hottest_tweet_by_user = None
     hottest_tweet_by_user = dict()
-    for user in tweets_by_user:
-        number_of_retweets = tweets_by_user[user][3]
-        if number_of_retweets == max_rt:
-            fitness_of_tweet = tweets_by_user[user][1]
-            is_feasible = tweets_by_user[user][2]
-            hottest_tweet_by_user[user] = [fitness_of_tweet, is_feasible, number_of_retweets]
-    print(hottest_tweet_by_user)
-
+    hottest_tweet_by_user = temp_hottest
 
 def empty_tweet():
     return [None] * pf.columnas
@@ -294,7 +389,7 @@ def main(argv):
             sys.exit()
         if opt in ("-i", "--ifile"):
             inputfile = arg
-        if opt in('-v'):
+        if opt in ('-v'):
             verbose = True
 
     run_mh(inputfile)
